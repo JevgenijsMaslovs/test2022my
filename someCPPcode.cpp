930227f61ab60b78ffbc6a7bfafcc74e418ab891
JevgenijsMaslovs <115455398+JevgenijsMaslovs@users.noreply.github.com>
2022-12-29 14:59:19 +0200

    new one man

--- someCPPcode.cpp
+++ someCPPcode.cpp
@@ -5,14 +5,20 @@ using namespace std;
 
 int main()
 {
-	string name, lastname;
-	int age;
+	string name,name2, lastname,lastname2;
+	int age, age2;
 	
 	cout<<"Ievadiet vardu: ";cin>>name;
 	cout<<"\nIevadiet uzvardu: ";cin>>lastname;
 	cout<<"\nIevadiet vecumu: ";cin>>age;
 	
 	cout<<"Hello my name is "<<name<<" "<<lastname<<" and i'm "<<age<<" old!"<<endl;
+	
+	cout<<"Ievadiet vardu: ";cin>>name2;
+	cout<<"\nIevadiet uzvardu: ";cin>>lastname2;
+	cout<<"\nIevadiet vecumu: ";cin>>age2;
+	
+	cout<<"Hello my name is "<<name2<<" "<<lastname2<<" and i'm "<<age2<<" old!"<<endl;
 
 
 	system("pause>nul");
